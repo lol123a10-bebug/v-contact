962156015856d38899cedc255218ea625ca155b6
lol123a10-bebug <ayubkhon.gulomov@gmail.com>
2022-11-15 18:51:05 +0500

    fixes;

--- src/utils/__mock/__mock-contacts.ts
+++ src/utils/__mock/__mock-contacts.ts
@@ -3,21 +3,35 @@ import type { TContact } from "../models/contact";
 const mockContacts: TContact[] = [
   {
     id: 1,
-    phone: "+998974803226",
+    phone: "+998971111111",
     fullName: "Ayyubkhon",
     email: "Test@g.co",
     tags: ["Key", "Lol"],
   },
   {
     id: 2,
-    phone: "+998974803226",
+    phone: "+998971111111",
     fullName: "Ayyubkhon",
     email: "Test@g.co",
     tags: ["Key", "Lol"],
   },
   {
     id: 3,
-    phone: "+998974803226",
+    phone: "+998971111111",
+    fullName: "Ayyubkhon",
+    email: "Test@g.co",
+    tags: ["Key", "Lol"],
+  },
+  {
+    id: 3,
+    phone: "+998971111111",
+    fullName: "Ayyubkhon",
+    email: "Test@g.co",
+    tags: ["Key", "Lol"],
+  },
+  {
+    id: 3,
+    phone: "+998971111111",
     fullName: "Ayyubkhon",
     email: "Test@g.co",
     tags: ["Key", "Lol"],
